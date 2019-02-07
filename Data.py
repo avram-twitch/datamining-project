84028d86ebae5e2d7766deafdc9a1ce9c8bc0a49
avram-twitch <avram.twitch@gmail.com>
2019-02-07 16:09:40 -0700

    h5 files read in chunks, and saved as pickled file

--- Data.py
+++ Data.py
@@ -1,24 +1,35 @@
 import h5py
+import pickle
+
 
 class Data:
 
     def __init__(self):
         pass
 
-    def create_data(self, h5_files, artist_file, track_file):
+    def create_data(self, h5_files, artist_file, track_file,
+                    chunks=500, dump_dir="./data/"):
 
-        self.track_to_id, \
-        self.artist_to_ids, \
-        self.id_to_artist_track = self._read_unique_tracks(track_file)
+        (self.track_to_id,
+         self.artist_to_ids,
+         self.id_to_artist_track) = self._read_unique_tracks(track_file)
 
         data = []
 
+        counter = 0
+        file_count = 0
         for f in h5_files:
+            counter += 1
             processed = self._process_file(f)
             data.append(processed)
-
-        self.data = data
-        return data
+            if counter % chunks == 0:
+                file_count += 1
+                out_file = dump_dir + "data_" + str(file_count) + ".pkl"
+                print("Saving %s songs to %s" % (str(chunks), out_file))
+                with open(out_file, 'wb') as g:
+                    pickle.dump(data, g)
+                del data
+                data = []
 
     def _process_file(self, fp):
 
@@ -27,16 +38,20 @@ class Data:
         track = self._get_track(track_id)
 
         h5 = self._read_h5_file(fp)
-        # Do stuff!
+        loudness = h5['analysis']['segments_loudness_max']
         h5.close()
 
-        return {'artist':artist, 'track':track, 'id':track_id}
+        out = {'artist': artist,
+               'track': track,
+               'id': track_id,
+               'loudness': loudness}
+
+        return out
 
     def _scrub_filepath_for_id(self, fp):
         length = len(fp.split('/'))
-        file_name = fp.split('/')[length - 1] 
-        return file_name.split('.h5')[0] # Assumes filename is track id
-
+        file_name = fp.split('/')[length - 1]
+        return file_name.split('.h5')[0]  # Assumes filename is track id
 
     def _get_artist(self, id):
         return self.id_to_artist_track[id]['artist']
@@ -90,8 +105,6 @@ class Data:
                 curr_item.append(id_1)
                 artist_to_ids[artist] = curr_item
                 track_to_id[track] = id_1
-                id_to_artist_track[id_1] = {'artist':artist, 'track':track}
+                id_to_artist_track[id_1] = {'artist': artist, 'track': track}
 
         return track_to_id, artist_to_ids, id_to_artist_track
-
-

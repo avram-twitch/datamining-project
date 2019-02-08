a433ed4e0e9ea62c42af3cdb61a5fd37a045df29
avram-twitch <avram.twitch@gmail.com>
2019-02-08 12:49:40 -0700

    Added process_raw_data file and data_collection report file

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 .PHONY: agg-data
 agg-data:
-	find ./MillionSongSubset/data/ -name "*.h5" -type f -exec mv {} ./data/ \;
+	find ./MillionSongSubset/data/ -name "*.h5" -type f -exec mv {} ./raw_data/ \;
+
+.PHONY: process-raw
+process-raw:
+	python3 ./process_raw_data.py
 
 .PHONY: lint 
 lint:
---
+++ process_raw_data.py
@@ -0,0 +1,16 @@
+from Data import Data
+import os
+
+if __name__ == '__main__':
+    x = Data()
+
+    unique_dir = "./MillionSongSubset/AdditionalFiles/"
+    unique_artist_fp = unique_dir + "subset_unique_artists.txt"
+    unique_tracks_fp = unique_dir + "subset_unique_tracks.txt"
+
+    data_dir = "./raw_data/"
+    all_files = os.listdir(data_dir)
+    for i in range(len(all_files)):
+        all_files[i] = data_dir + all_files[i]
+
+    data = x.create_data(all_files, unique_artist_fp, unique_tracks_fp)

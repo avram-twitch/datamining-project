71e683b85a5b6c51d4979b0327e2b60ef2c0e113
avram-twitch <avram.twitch@gmail.com>
2019-02-18 16:35:51 -0700

    Added documentation to Data object

--- Data.py
+++ Data.py
@@ -3,12 +3,29 @@ import pickle
 
 
 class Data:
+    """
+    Data processing object.
+
+    Processes raw h5 data files from Million Song Dataset (herafter MSD)
+    """
 
     def __init__(self):
         pass
 
     def create_data(self, h5_files, artist_file, track_file,
                     chunks=500, dump_dir="./data/"):
+        """
+        Processes h5 files into pickled chunks.
+
+        :param h5_files: list of paths to h5 files to be processed.
+        :param artist_file: path to artist info csv file.
+        :param track_file: path to track info csv file
+        :param chunks: number of h5 files to be processed before
+                they are downloaded as a pickled file
+        :param dump_dir: folder path to directory where pickled files
+                will be stored
+        :returns: None
+        """
 
         (self.track_to_id,
          self.artist_to_ids,
@@ -32,6 +49,12 @@ class Data:
                 data = []
 
     def _process_file(self, fp):
+        """
+        Processes a single h5 file into a dictionary object.
+
+        :param fp: Filepath to h5 file to be processed.
+        :returns: Dictionary with keys to each variable extracted.
+        """
 
         track_id = self._scrub_filepath_for_id(fp)
         artist = self._get_artist(track_id)
@@ -49,19 +72,44 @@ class Data:
         return out
 
     def _scrub_filepath_for_id(self, fp):
+        """
+        Extracts track id from filepath. (The MSD format has the track id in the filename)
+
+        :param fp: Filepath of the h5 file to be processed.
+        :return: Track ID
+        """
+
+        # TODO: Does the track id exist in each h5 file? May be more
+        #       robust to get id there
+
         length = len(fp.split('/'))
         file_name = fp.split('/')[length - 1]
         return file_name.split('.h5')[0]  # Assumes filename is track id
 
     def _get_artist(self, id):
+        """
+        Given an id, returns the associated artist
+
+        :param id: Track id
+        :return: Name of artist
+        """
         return self.id_to_artist_track[id]['artist']
 
     def _get_track(self, id):
+        """
+        Given an id, returns the associated track
+
+        :param id: Track id
+        :return: Name of track
+        """
         return self.id_to_artist_track[id]['track']
 
     def _read_h5_file(self, fp):
         """
         Wrapper to read an h5 file
+
+        :param fp: Filepath to h5 file
+        :return: h5 File object (read only)
         """
         h5 = h5py.File(fp, 'r')
         return h5
@@ -70,9 +118,10 @@ class Data:
         """
         Extracts id-artists mappings from unique_artists file
 
-        returns two dictionaries, one with id as key, and the
-            other with artist as key.
-            Note that an artist may have multiple ids
+        :param fp: Filepath to unique artists file
+        :returns: Two dictionaries, one with id as key, and the
+                other with artist as key.
+                Note that an artist may have multiple ids
         """
 
         artist_to_id = {}
@@ -91,8 +140,9 @@ class Data:
         """
         Extracts id-artists mappings from unique_tracks file
 
-        returns two dictionaries, one with id as key, and the
-            other with track as key.
+        :param fp: Filepath to unique tracks csv file
+        :return: two dictionaries, one with id as key, and the
+                other with track as key.
         """
 
         track_to_id = {}

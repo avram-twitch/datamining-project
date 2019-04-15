cfbbcf9399645d59768dcf7e2efa54fb364b912e
avram-twitch <avram.twitch@gmail.com>
2019-04-15 16:06:21 -0600

    Wrote Final Report

--- analyze.py
+++ analyze.py
@@ -41,6 +41,20 @@ def plot_sds_of_top_n_variables(fp, analyzer, var, values, ks, ns):
     plotter = Plotter()
     plotter.plot_sds_of_top_n_variables(fp, analyzer, var, values, ks, ns, False)
 
+def plot_sds_away(fp, analyzer, var, values, ks, ns):
+    plotter = Plotter()
+    plotter.plot_sds_away(fp, analyzer, var, values, ks, ns, False)
+
+def plot_percs_away(fp, analyzer, var, values, ks, ns):
+    plotter = Plotter()
+    plotter.plot_percs_away(fp, analyzer, var, values, ks, ns, False)
+
+def plot_single_run(run, top_terms, top_artists, decades):
+    plotter = Plotter()
+    plotter.plot_single_run("./results/terms_cluster.png", run, 'term', top_terms)
+    plotter.plot_single_run("./results/decades_cluster.png", run, 'decade', decades)
+    plotter.plot_single_run("./results/artists_cluster.png", run, 'artist', top_artists)
+
 if __name__=='__main__':
 
     ks = [3, 4, 5]
@@ -56,6 +70,17 @@ if __name__=='__main__':
     top_artists = [x[0] for x in top_artists]
     decades = list(range(1920, 2020, 10))
     decades = [str(x) for x in decades]
-    plot_sds_of_top_n_variables("./results/terms.png", analyzer, 'term', top_terms, ks, ns)
-    plot_sds_of_top_n_variables("./results/artists.png", analyzer, 'artist', top_artists, ks, ns)
-    plot_sds_of_top_n_variables("./results/decades.png", analyzer, 'decade', decades, ks, ns)
+    trunc_decades = list(range(1950, 2020, 10))
+    trunc_decades = [str(x) for x in trunc_decades]
+#    plot_sds_of_top_n_variables("./results/sd_terms.png", analyzer, 'term', top_terms, ks, ns)
+#    plot_sds_of_top_n_variables("./results/sd_artists.png", analyzer, 'artist', top_artists, ks, ns)
+#    plot_sds_of_top_n_variables("./results/sd_decades.png", analyzer, 'decade', decades, ks, ns)
+#    plot_sds_away("./results/sd_away_terms.png", analyzer, 'term', top_terms, ks, ns)
+#    plot_sds_away("./results/sd_away_artists.png", analyzer, 'artist', top_artists, ks, ns)
+#    plot_sds_away("./results/sd_away_decades.png", analyzer, 'decade', decades, ks, ns)
+#    plot_percs_away("./results/perc_away_terms.png", analyzer, 'term', top_terms, ks, ns)
+#    plot_percs_away("./results/perc_away_artists.png", analyzer, 'artist', top_artists, ks, ns)
+#    plot_percs_away("./results/perc_away_decades.png", analyzer, 'decade', decades, ks, ns)
+#    plot_percs_away("./results/perc_away_trunc_decades.png", analyzer, 'decade', trunc_decades, ks, ns)
+
+    plot_single_run(analyzer.get_c_run(5, 4, large=False), top_terms, top_artists, decades)
--- src/Plotter.py
+++ src/Plotter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import copy
 
 
 class Plotter:
@@ -8,6 +9,49 @@ class Plotter:
     def __init__(self):
         self.xlims = [-1.1, 1.1]
         self.ylims = [-1.1, 1.1]
+
+    def plot_single_run(self, plot_fp, run, var, values, diff=True):
+        n_clusters = len(run)
+        data = np.zeros((n_clusters + 1, len(values)))
+        cluster_labels = ['all']
+        i = 0
+        for cluster in run:
+            cluster_labels.append("C{}\nn={}".format(i+1, len(cluster)))
+            i += 1
+
+        i = 0
+        for val in values:
+            j = 1
+            all_count = 0
+            all_filt = 0
+            for cluster in run:
+                curr = self._filter_by(cluster, (var, val))
+                data[j, i] = float(len(curr)) / len(cluster) * 100
+                all_count += len(cluster)
+                all_filt += len(curr)
+                j += 1
+
+            data[0, i] = float(all_filt) / all_count * 100
+
+            i += 1
+
+        if diff:
+            percs = copy.deepcopy(data)
+            labels = np.empty((n_clusters + 1, len(values)), dtype="<U15")
+            for i in range(len(values)):
+                curr_pop = data[0, i]
+                for j in range(len(run) + 1):
+                    data[j, i] = data[j, i] - curr_pop
+                    labels[j, i] = "{:.1f}\n({:.1f}%)".format(data[j, i], percs[j, i])
+
+
+        ax = sns.heatmap(data, xticklabels=values, yticklabels=cluster_labels, annot=labels, linewidths=.5, fmt="")
+        ax.set(xlabel=var, ylabel="run (K_N)")
+        plt.title("Variable Representation Differences from Population ({}s)".format(var))
+        fig = plt.gcf()
+        fig.set_size_inches(16, 10)
+        plt.savefig(plot_fp)
+        plt.clf()
 
     def plot_sds_of_top_n_variables(self, plot_fp, analyzer, var, values, ks, ns, show=False):
         data = np.zeros((len(ks) * len(ns), len(values)))
@@ -26,12 +70,69 @@ class Plotter:
                 key = self._create_kn_key(k, n)
                 keys.append(key)
 
-        ax = sns.heatmap(data, xticklabels=values, yticklabels=keys)
+        ax = sns.heatmap(data, xticklabels=values, yticklabels=keys, annot=True, fmt='.0f',linewidths=.5)
+        ax.set(xlabel=var, ylabel="run (K_N)")
+        plt.title("Std. Dev. of Cluster Percentages ({}s)".format(var))
+        fig = plt.gcf()
+        fig.set_size_inches(16, 10)
         plt.savefig(plot_fp)
         if show:
             plt.show()
         plt.clf()
 
+    def plot_sds_away(self, plot_fp, analyzer, var, values, ks, ns, show=False):
+        data = np.zeros((len(ks) * len(ns), len(values)))
+        i = 0
+        for v in values:
+            sds = self._get_max_sds_away(analyzer, var, v, ks, ns)
+            j = 0
+            for sd in sds:
+                data[j][i] = sd
+                j += 1
+            i += 1
+
+        keys = []
+        for k in ks:
+            for n in ns:
+                key = self._create_kn_key(k, n)
+                keys.append(key)
+
+        ax = sns.heatmap(data, xticklabels=values, yticklabels=keys, annot=True, fmt='.0f',linewidths=.5)
+        ax.set(xlabel=var, ylabel="run (K_N)")
+        plt.title("Max Cluster Std. Dev. away from Population mean ({}s)".format(var))
+        fig = plt.gcf()
+        fig.set_size_inches(16, 10)
+        plt.savefig(plot_fp)
+        if show:
+            plt.show()
+        plt.clf()
+
+    def plot_percs_away(self, plot_fp, analyzer, var, values, ks, ns, show=False):
+        data = np.zeros((len(ks) * len(ns), len(values)))
+        i = 0
+        for v in values:
+            percs = self._get_max_percs_away(analyzer, var, v, ks, ns)
+            j = 0
+            for perc in percs:
+                data[j][i] = perc
+                j += 1
+            i += 1
+
+        keys = []
+        for k in ks:
+            for n in ns:
+                key = self._create_kn_key(k, n)
+                keys.append(key)
+
+        ax = sns.heatmap(data, xticklabels=values, yticklabels=keys, annot=True, fmt='.0f',linewidths=.5)
+        ax.set(xlabel=var, ylabel="run (K_N)")
+        plt.title("Max Perc. away from Population mean ({}s)".format(var))
+        fig = plt.gcf()
+        fig.set_size_inches(16, 10)
+        plt.savefig(plot_fp)
+        if show:
+            plt.show()
+        plt.clf()
 
     def _get_sds(self, analyzer, var, val, ks, ns):
         out = []
@@ -42,6 +143,68 @@ class Plotter:
                 run = runs[run_key]
                 sd = self._get_std_dev_from_counts(run)
                 out.append(sd)
+
+        return out
+
+    def _get_max_sds_away(self, analyzer, var, val, ks, ns):
+        out = []
+        runs = analyzer.get_all_cluster_counts(var, val, True)
+        for k in ks:
+            for n in ns:
+                run_key = self._create_kn_key(k, n)
+                run = runs[run_key]
+                sd = self._get_std_dev_from_counts(run)
+                sds_away = self._get_sds_away(run, sd)
+                out.append(max(sds_away))
+
+        return out
+
+    def _get_max_percs_away(self, analyzer, var, val, ks, ns):
+        out = []
+        runs = analyzer.get_all_cluster_counts(var, val, True)
+        for k in ks:
+            for n in ns:
+                run_key = self._create_kn_key(k, n)
+                run = runs[run_key]
+                percs_away = self._get_perc_away(run)
+                out.append(max(percs_away) * 100)
+
+        return out
+
+    def _get_perc_away(self, run):
+        all_count = 0
+        all_total = 0
+        out = []
+        for count, total in run:
+            all_count += count
+            all_total += total
+            out.append(float(count) / total)
+
+        all_perc = float(all_count) / all_total
+
+        i = 0
+        for count, total in run:
+            out[i] = abs(out[i] - all_perc)
+            out[i] = out[i] / all_perc
+            i += 1
+
+        return out
+
+    def _get_sds_away(self, run, sd):
+        all_count = 0
+        all_total = 0
+        out = []
+        for count, total in run:
+            all_count += count
+            all_total += total
+            out.append(float(count) / total)
+
+        all_perc = float(all_count) / all_total
+
+        i = 0
+        for count, total in run:
+            out[i] = abs(out[i] - all_perc) / sd
+            i += 1
 
         return out
 
@@ -67,7 +230,7 @@ class Plotter:
 
         extent = (min_n, max_n, min_k, max_k)
         # plt.imshow(results, cmap='hot', interpolation='nearest', extent=extent)
-        ax = sns.heatmap(results, xticklabels=ns, yticklabels=ks)
+        ax = sns.heatmap(results, xticklabels=ns, yticklabels=ks, annot=True)
 
         plt.savefig(plot_fp)
         if show:
